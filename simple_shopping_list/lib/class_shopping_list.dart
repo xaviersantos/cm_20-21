c884c2141708a36e20700c551206a424254703ba
jorgefff <jorgeffaustino@gmail.com>
2020-11-26 18:35:20 +0000

    Removed old files

--- simple_shopping_list/lib/class_shopping_list.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-
-class ShoppingList {
-  final String title;
-  final String description;
-
-  ShoppingList(this.title, this.description);
-}
