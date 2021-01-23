f1f5f8929047734f8d3572d78c6e31e03ea3f7b6
jorgefff <jorgeffaustino@gmail.com>
2021-01-23 05:54:33 +0000

    mapa com localizaçao fixa

--- project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapFragment.java
+++ project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapFragment.java
@@ -1,6 +1,10 @@
 package pt.ua.cm.myshoppinglist.ui.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,11 +27,25 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import pt.ua.cm.myshoppinglist.R;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback {
+import static android.content.Context.LOCATION_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
 
-    private MapViewModel mMapViewModel;
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    long LOCATION_REFRESH_TIME = 1000;// milisecs
+    float LOCATION_REFRESH_DISTANCE = 10;  // meters
+    public static final String LOCATION_SERVICE = "location";
+
     private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    private MapViewModel mMapViewModel;
     private SupportMapFragment mMapFragment;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            System.out.println("-----------------------\n\n"+location.toString());
+        }
+    };
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
@@ -34,15 +53,16 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         View root = inflater.inflate(R.layout.fragment_map, container, false);
 //        final TextView textView = root.findViewById(R.id.text_map);
 
-        mMapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
+//        mMapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
 //                textView.setText(s);
-            }
-        });
+//            }
+//        });
 
         mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
+
         return root;
     }
 
@@ -56,10 +76,4 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            //your code here
-        }
-    };
 }
