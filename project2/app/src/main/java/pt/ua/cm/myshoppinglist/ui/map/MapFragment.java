feba2cb242043c6bb0528d00014312374cee44eb
jorgefff <jorgeffaustino@gmail.com>
2021-01-24 19:12:52 +0000

    Mapa mostra localizaçao

--- project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapFragment.java
+++ project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapFragment.java
@@ -1,79 +1,182 @@
 package pt.ua.cm.myshoppinglist.ui.map;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 import pt.ua.cm.myshoppinglist.R;
 
-import static android.content.Context.LOCATION_SERVICE;
-import static androidx.core.content.ContextCompat.getSystemService;
+import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
-    long LOCATION_REFRESH_TIME = 1000;// milisecs
+    long LOCATION_REFRESH_TIME = 1000;      // milisecs
+    long LOCATION_REFRESH_TIME_FAST = 500;
     float LOCATION_REFRESH_DISTANCE = 10;  // meters
-    public static final String LOCATION_SERVICE = "location";
 
-    private GoogleMap mMap;
-    private LocationManager mLocationManager;
     private MapViewModel mMapViewModel;
-    private SupportMapFragment mMapFragment;
 
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(final Location location) {
-            System.out.println("-----------------------\n\n"+location.toString());
-        }
-    };
+    private boolean isCamInitPosSetup = false;
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMapViewModel = new ViewModelProvider(this).get(MapViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_map, container, false);
-//        final TextView textView = root.findViewById(R.id.text_map);
 
+//        final TextView textView = root.findViewById(R.id.text_map);
 //        mMapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
+////                textView.setText(s);
+//            }
+//        });
+//        mMapViewModel.getMap().observe(getViewLifecycleOwner(), new Observer<GoogleMap>() {
+//            @Override
+//            public void onChanged(GoogleMap googleMap) {
+//
 //            }
 //        });
 
+        // Init map
         mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
+
+
 
         return root;
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d("MAP","inside onMapReady");
         mMap = googleMap;
+        if(checkPermissions()) {
+            mMap.setMyLocationEnabled(true); // Self location button
+        }
+        startLocationUpdates();
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 
+    // Trigger new location updates at interval
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(LOCATION_REFRESH_TIME);
+        mLocationRequest.setFastestInterval(LOCATION_REFRESH_TIME_FAST);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(getActivity());
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+
+        // Constant updates
+//        getFusedLocationProviderClient(getActivity()).requestLocationUpdates(
+//                mLocationRequest,
+//                new LocationCallback() {
+//                    @Override
+//                    public void onLocationResult(LocationResult locationResult) {
+//                        // do work here
+//                        Log.d("MAP","inside onLocationResult");
+//                        if (locationResult != null && mMap != null && !isCamInitPosSetup) {
+//                            double lat = locationResult.getLastLocation().getLatitude();
+//                            double lng = locationResult.getLastLocation().getLongitude();
+//                            LatLng latLng = new LatLng(lat,lng);
+//                            moveCamera(latLng);
+//                            isCamInitPosSetup = true;
+//                        }
+//                    }
+//                },
+//                Looper.myLooper());
+
+        // Get current position and update camera
+        LocationServices.getFusedLocationProviderClient(getActivity())
+                .getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                Log.d("MAP","inside onSuccess location");
+                if (location != null && mMap != null && !isCamInitPosSetup) {
+                    double lat = location.getLatitude();
+                    double lng = location.getLongitude();
+                    LatLng latLng = new LatLng(lat,lng);
+                    moveCamera(latLng);
+                    isCamInitPosSetup = true;
+                }
+            }
+        });
+    }
+
+
+    private boolean checkPermissions() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            requestPermissions();
+            return false;
+        }
+    }
+
+    private void requestPermissions() {
+        int REQUEST_FINE_LOCATION = 1001;
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_FINE_LOCATION);
+    }
+
+    private void moveCamera(LatLng latLng) {
+        if (mMap == null) {
+            return;
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+    }
 }
--- project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapViewModel.java
+++ project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapViewModel.java
@@ -4,16 +4,25 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.maps.GoogleMap;
+
 public class MapViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<GoogleMap> mMap;
 
     public MapViewModel() {
         mText = new MutableLiveData<>();
+        mMap = new MutableLiveData<>();
+
 //        mText.setValue("Items Location");
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<GoogleMap> getMap() {
+        return mMap;
     }
 }
