accd598d8fa64f9360c36329d41b536c9cab374b
jorgefff <jorgeffaustino@gmail.com>
2021-01-24 20:33:37 +0000

    update mapa

--- project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapFragment.java
+++ project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapFragment.java
@@ -5,30 +5,22 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -36,10 +28,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-
 import pt.ua.cm.myshoppinglist.R;
-
-import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
     long LOCATION_REFRESH_TIME = 1000;      // milisecs
@@ -59,25 +48,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         View root = inflater.inflate(R.layout.fragment_map, container, false);
 
-//        final TextView textView = root.findViewById(R.id.text_map);
-//        mMapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-////                textView.setText(s);
-//            }
-//        });
-//        mMapViewModel.getMap().observe(getViewLifecycleOwner(), new Observer<GoogleMap>() {
-//            @Override
-//            public void onChanged(GoogleMap googleMap) {
-//
-//            }
-//        });
-
         // Init map
         mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
-
-
 
         return root;
     }
@@ -85,18 +58,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d("MAP","inside onMapReady");
         mMap = googleMap;
         if(checkPermissions()) {
             mMap.setMyLocationEnabled(true); // Self location button
         }
         startLocationUpdates();
-
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
     }
 
     // Trigger new location updates at interval
@@ -117,25 +83,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
         SettingsClient settingsClient = LocationServices.getSettingsClient(getActivity());
         settingsClient.checkLocationSettings(locationSettingsRequest);
-
-        // Constant updates
-//        getFusedLocationProviderClient(getActivity()).requestLocationUpdates(
-//                mLocationRequest,
-//                new LocationCallback() {
-//                    @Override
-//                    public void onLocationResult(LocationResult locationResult) {
-//                        // do work here
-//                        Log.d("MAP","inside onLocationResult");
-//                        if (locationResult != null && mMap != null && !isCamInitPosSetup) {
-//                            double lat = locationResult.getLastLocation().getLatitude();
-//                            double lng = locationResult.getLastLocation().getLongitude();
-//                            LatLng latLng = new LatLng(lat,lng);
-//                            moveCamera(latLng);
-//                            isCamInitPosSetup = true;
-//                        }
-//                    }
-//                },
-//                Looper.myLooper());
 
         // Get current position and update camera
         LocationServices.getFusedLocationProviderClient(getActivity())
--- project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapViewModel.java
+++ project2/app/src/main/java/pt/ua/cm/myshoppinglist/ui/map/MapViewModel.java
@@ -5,24 +5,25 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import pt.ua.cm.myshoppinglist.R;
 
 public class MapViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
-    private MutableLiveData<GoogleMap> mMap;
+//    private MutableLiveData<GoogleMap> mMap;
+
+    private GoogleMap mMap;
 
     public MapViewModel() {
-        mText = new MutableLiveData<>();
-        mMap = new MutableLiveData<>();
-
 //        mText.setValue("Items Location");
     }
+
+
 
     public LiveData<String> getText() {
         return mText;
     }
 
-    public LiveData<GoogleMap> getMap() {
-        return mMap;
-    }
 }
